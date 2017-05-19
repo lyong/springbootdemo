ae9fdc01934b777c81e172c21fbdb7281ce582bc
lyong <lyong8830@hotmail.com>
2017-05-19 18:12:11 +0800

    create demo

---
+++ src/main/java/xyz/lvyong/blog/Application.java
@@ -0,0 +1,24 @@
+package xyz.lvyong.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * @Author lvyong
+ */
+@RestController
+@EnableAutoConfiguration
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
